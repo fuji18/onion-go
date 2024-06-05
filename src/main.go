34113293ae549c8d4975597252c1b42615e87b5c
kazu <fuji18.create@gmail.com>
2024-06-06 00:23:39 +0900

    feat:conect mysql using gorn

--- src/main.go
+++ src/main.go
@@ -3,16 +3,34 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func main() {
+	dsn := "root:password@tcp(mysql_db)/dev?charset=utf8mb4&parseTime=True&loc=Local"
+	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	db.AutoMigrate(&Product{})
+
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	//Read
+	var product Product
+	db.First(&product, 1)
+
 	router := gin.Default()
 	router.Use(cors.Default())
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"id":   "geajpgeapga",
-			"name": "テスト太郎",
+			"code": product,
 		})
 	})
 	router.Run(":8000")
